8c9eb2cfb79009c965bd7988b68e7d86de6d8472
maebh2021 <88852417+maebh2021@users.noreply.github.com>
2021-09-15 21:29:17 +0100

    updated 1 file with api test1.py

--- test1.py
+++ test1.py
@@ -1,5 +1,23 @@
-name = "maebh"
-print(name)
-a = 2
-b = 3
-print(a + b)
+# Importing basic Python libraries for data cleaning and visualisation
+
+import pandas as pd
+import requests
+import matplotlib.pyplot as plt
+import numpy as np
+import seaborn as sns
+
+# Retrieve data from online API showing daily currency fx rates for USD
+request = requests.get("https://open.er-api.com/v6/latest/USD")
+print(request.status_code)
+print(request.text)
+data = request.json()
+print(data)
+# Convert data into Pandas Dataframe
+df = pd.DataFrame(data)
+# Get a list of all column names
+print(df.columns)
+# Select columns needed: time_last_update_utc, base_code, rates and save as DataFrame fx_rates
+fx_rates = df[["time_last_update_utc", "base_code", "rates"]]
+print(fx_rates)
+
+# Import csv file into pandas DataFrame
